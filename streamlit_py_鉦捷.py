0a14bdc0ee9b3a1a1731c47ad83e5e853e979185
FaustRen <82464741+FaustRen@users.noreply.github.com>
2022-10-14 18:31:37 +0800

    Update streamlit_py_鉦捷.py

--- streamlit_py_鉦捷.py
+++ streamlit_py_鉦捷.py
@@ -18,54 +18,6 @@ st.set_page_config(
 st.title('階段1: 總重量x區費率x0.9')
 
 # # ## 設定背景
-# @st.cache(allow_output_mutation=True)
-# def get_base64_of_bin_file(bin_file):
-#     with open(bin_file, 'rb') as f:
-#         data = f.read()
-#     return base64.b64encode(data).decode()
-
-# def set_png_as_page_bg(png_file):
-#     bin_str = get_base64_of_bin_file(png_file)
-#     page_bg_img = '''
-#     <style>
-#     body {
-#     background-image: url("data:image/png;base64,%s");
-#     background-size: cover;
-#     }
-#     </style>
-#     ''' % bin_str
-    
-#     st.markdown(page_bg_img, unsafe_allow_html=True)
-#     return
-
-# set_png_as_page_bg('background.png')
-
-
-# # https://s30876.pcdn.co/wp-content/uploads/Japan-e1634207070862-1170x630.jpg.webp
-# page_bg_img = '''
-# <style>
-# body {
-# background-image: url("https://s30876.pcdn.co/wp-content/uploads/Japan-e1634207070862-1170x630.jpg.webp");
-# background-size: cover;
-# }
-# </style>
-# '''
-# st.markdown(page_bg_img, unsafe_allow_html=True)
-
-st.markdown(
-    """
-    <style>
-    .reportview-container {
-        background: url("https://images.app.goo.gl/LFCobouKtT7oZ7Qv7")
-    }
-   .sidebar .sidebar-content {
-        background: url("https://images.app.goo.gl/LFCobouKtT7oZ7Qv7")
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 
 
 
